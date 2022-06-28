719012a9c3170a76b1f0bd356b414c326f80309d
unknown <shubh@shubh.com>
2022-06-28 17:41:57 +0530

    new files

--- src/Components/BookARepair/Repair.js
+++ src/Components/BookARepair/Repair.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import './index.css'
 import FirstPage from "./BookARepairFiles/FirstPage"
 import SecondPage from "./BookARepairFiles/SecondPage"
 import ThirdPage from "./BookARepairFiles/ThirdPage"
 
 function Repair() {
   return (
-    <div>
+    <div className='repair'>
         <FirstPage />
         <SecondPage />
         <ThirdPage />
--- src/Components/Homepage/Header2.js
+++ src/Components/Homepage/Header2.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
 import "./Header2.css";
 import "./Header1.css";
 import { Link } from "react-router-dom";
