ab7ee638f66c26025f7a66ed857ce4cbc59082a8
ritrikrohra <ritrikrohra1230456@gmail.com>
2022-07-05 11:10:47 +0530

    more functionalities added

--- src/Components/BookARepair/BookARepairFiles/DetailsSection.js
+++ src/Components/BookARepair/BookARepairFiles/DetailsSection.js
@@ -22,7 +22,6 @@ export default function DetailsSection() {
   const [brandName, setBrandName] = useState("");
   const [colourName, setColourName] = useState("");
   const [modelName, setModelName] = useState("");
-  const [issueName, setIssueName] = useState("");
   const [timeSlot, setTimeSlot] = useState("");
   const [date, setDate] = useState("");
   const [multiIssue, setMultiIssue] = useState([]);
@@ -38,7 +37,7 @@ export default function DetailsSection() {
       mobile_brand : brandName,
       mobile_model : modelName,
       mobile_color : colourName,
-      issues : issueName,
+      issues : multiIssue,
       repair_date : date,
       time_slot_id : timeSlot
     },
@@ -66,19 +65,44 @@ export default function DetailsSection() {
   }
   const handleColour = (e) => {
     setColourName(e.target.value);
-    console.log(e.target.value);
-  }
-  const handleIssue = (e) => {
-    setIssueName(e.target.value);
   }
   const handleDate = (e) => {
-    setDate(e.target.value);
+    const yyyy_mm_dd = e.target.value;
+    const year = yyyy_mm_dd.slice(0,4);
+    const month = yyyy_mm_dd.slice(5,7);
+    const day = yyyy_mm_dd.slice(8,10);
+    const dd_mm_yyyy = day+"-"+month+"-"+year;
+    setDate(dd_mm_yyyy);
   }
   const handleTime = (e) => {
     setTimeSlot(e.target.value);
   }
-  const handleMultiIssue = (e) => {
-    console.log(e.target.value);
+  const handleIssueCheck = () =>{
+    const finalCheckedArray = [];
+    const checkedValue = document.querySelectorAll('.issueItem:checked');
+    checkedValue.forEach((e)=>{
+      finalCheckedArray.push(e.value);
+      setMultiIssue(finalCheckedArray);
+    })
+  }
+
+  // const handleIssueClick = (current) =>{
+  //   console.log(current.target.value);
+  //   const label = document.querySelectorAll(".issueLabel");
+  //   label.forEach((e)=>{
+  //     const checkbox = e.firstElementChild;
+  //     // console.log(e.for);
+  //     // if(checkbox.checked == true){
+  //     //   checkbox.checked = false;
+  //     // }
+  //     // else{
+  //     //   checkbox.checked = true;
+  //     // }
+  //   })
+  // }
+
+  const handleIssueClick = (e) =>{
+    console.log(e);
   }
 
   useEffect(() => {
@@ -97,28 +121,19 @@ export default function DetailsSection() {
       });
   }, []);
 
-  // const issues = ["hell0","byebye"];
-
-  // const issues = issue.map((data) => {
-  //   value : data.title,
-  //   label : data.title
-  // })
-
-  // const issues = [
-  //   {
-  //     value: "lolvalue1",
-  //     label: "lollabel1"
-  //   },
-  //   {
-  //     value: "lolvalue2",
-  //     label: "lollabel2"
-  //   }
-  // ]
+  const handleIssueDisplay = () =>{
+    const issueList = document.querySelector(".issueList");
+    if(issueList.classList.contains("issueDisplay")){
+      issueList.style.cssText = "display:block";
+    }
+    else{
+      issueList.style.cssText = "display:none";
+    }
+    issueList.classList.toggle("issueDisplay");
+  }
 
   return (
-   
     <Details className="details" onSubmit={handleSubmit}>
-        
       <Brand required="required">
         <label>Brand</label>
         <select onChange={handleBrand} name="brand">
@@ -159,17 +174,18 @@ export default function DetailsSection() {
         </select>
       </Colour>
       <Issue>
-        <label>Issue with phone</label>
-        <select name="issue" className="select" onChange={handleIssue} isMulti>
-          <option value="" selected disabled>
-            Choose an Issue
-          </option>
-          {issue.map((data, index) => (
-            <option key={index} value={data.title} >
-              {data.title}
-            </option>
-          ))}
-        </select>
+        <label className="issueLabelMain" >Issue with phone</label>
+        <input className="issueInputMain" type="text" placeholder="Choose an Issue" onClick={handleIssueDisplay}/>
+        <IssueList className="issueList issueDisplay">
+          {
+            issue.map((data, index) => (
+              <label for={data.title} value={data.title} id={index} className="issueLabel" onClick={handleIssueClick}>
+                <input type="checkbox" id={index} value={data.title} className="issueItem" onChange={handleIssueCheck} />
+                {data.title}<br/>
+              </label>
+            ))
+          }
+        </IssueList>
       </Issue>
       <Date>
         <label>Repairing Date</label>
@@ -188,21 +204,6 @@ export default function DetailsSection() {
           ))}
         </select>
       </Time>
-      {/* <Select isMulti options={lol}></Select>
-      <Multiselect
-        isObject={false}
-        onKeyPressFn={function noRefCheck(){}}
-        onRemove={(e)=>{console.log(e)}}
-        onSearch={function noRefCheck(){}}
-        onSelect={function noRefCheck(){}}
-        options={[
-          'Option 1',
-          'Option 2',
-          'Option 3',
-          'Option 4',
-          'Option 5'
-        ]}
-      /> */}
       <Text>
         <p>Fill Details And Get Your Mobile Repaired At Your Doorstep</p>
       </Text>
@@ -235,6 +236,7 @@ const Brand = styled.div`
     width: 80%;
     text-align: left;
     margin-bottom: 3px;
+    z-index : 1;
   }
   input {
     width: 80%;
@@ -253,9 +255,56 @@ const Brand = styled.div`
 `;
 const Model = styled(Brand)``;
 const Colour = styled(Brand)``;
-const Issue = styled(Brand)``;
 const Date = styled(Brand)``;
 const Time = styled(Brand)``;
+const Issue = styled.div`
+  width: 40%;
+  margin: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position : relative;
+  .issueLabelMain {
+    font-size: 1.3rem;
+    font-weight: bolder;
+    display: inline-block;
+    width: 80%;
+    text-align: left;
+    margin-bottom: 3px;
+  }
+  .issueInputMain {
+    width: 80%;
+    height: 40px;
+    border-radius: 10px;
+    padding: 0 10px;
+    border: 1.5px solid #561d9c;
+  }
+  .issueDisplay{
+    display: none;
+  }
+`;
+const IssueList = styled.div`
+width : 80%;
+border: 1px solid black;
+box-shadow: 0 0 5px grey;
+position : absolute;
+top : 100%;
+z-index : 10;
+background-color : white;
+input{
+  margin-right : 10px;
+}
+label{
+  display : inline-block;
+  width : 100%;
+  font-size : 14px;
+  padding : 0 10px;
+}
+label:hover{
+  background-color : #1e90ff;
+  color : white;
+}
+`
 const Text = styled.div`
   width: 40%;
   text-align: center;
