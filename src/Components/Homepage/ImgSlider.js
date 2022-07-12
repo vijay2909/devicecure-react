6809af7605f7af324d94853fa4812038f9663f54
ritrikrohra <ritrikrohra1230456@gmail.com>
2022-07-12 18:51:14 +0530

    carousel

--- src/Components/Homepage/ImgSlider.js
+++ src/Components/Homepage/ImgSlider.js
@@ -1,34 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ImgSlider.css";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import styled from "styled-components";
+import axios from "axios";
 
 function ImgSlider() {
+
+  const [banner, setBanner] = useState([]);
+
+  useEffect(()=>{
+    axios.get("https://staging.devicecure.in/api/banners")
+    .then((res)=>{
+      setBanner(res.data.data);
+      // console.log(res.data.data);
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  },[])
+
   let settings = {
     dots: true,
-    Infinite: true,
+    infinite: true,
     speed: 500,
     slidesToShow: 1,
-    slidersToScroll: 1,
-    autoplay: true,
+    slidesToScroll: 1
   };
-// slider dimentions--1440/400px
-
   return <Slider className="ImgSlider" {...settings}>
-     <div className="ImgSlider__division">
-      <img className="ImgSlider__img" src=""  alt="Image 1"/>
-    </div>
-    <div className="ImgSlider__division">
-      <img className="ImgSlider__img" src=""  alt="Image 2"/>
-    </div>
-    <div className="ImgSlider__division">
-      <img className="ImgSlider__img" src=""  alt="Image 3"/>
-    </div>
-    <div className="ImgSlider__division">
-      <img className="ImgSlider__img" src=""  alt="Image 4"/>
-    </div>
+    {
+      banner.map((data, idx)=>
+        <MainDiv className="ImgSlider__division">
+          <Image className="ImgSlider__img" src={`${data.image.url_standard}`}  alt={`${data.image.image_url}`}/>
+        </MainDiv>
+      )
+    }
   </Slider>;
 }
+const MainDiv = styled.div`
+width : 100%;
+height : 40vh;
+`
+const Image = styled.img`
+widht : 100%;
+height : 100%;
+`
 
 export default ImgSlider;
